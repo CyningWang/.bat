43f6d316ff8a41ab4663c8cd61506eaba9b49a24
Black-White <1652545601@qq.com>
2018-11-07 09:53:35 +0800

    批处理替换多文件的相同部分

--- edit.bat
+++ edit.bat
@@ -1,2 +1,23 @@
 @echo off&setlocal EnableDelayedExpansion
 cd /d "工作目录"  
+for %%a in (*.html) do (
+  (for /f "delims=" %%b in ('type "%%a"') do (
+    set "str=%%b"
+    if /i "!str:指定替换部分=!" == "%%b" (
+      if defined . set .=&type replace.list & echo,
+      echo, %%b
+    ) else (
+      set .=.
+    )
+  ))>tmp
+  move tmp "%%a"
+)
+
+/* 注释 */
+//  cd /d 直接进入指定目录，跳过盘符转换
+//  /i 忽略大小写
+//  !str:指定替换部分=!   替换为空，替换后与原行字符串比较，是否相同，相同意味着，没有指定内容，不同，意味着有指定内容
+//  set .=. 设置标志符变量，表示已经找到相同部分
+//  if defined . 判断是否已经定义 . 变量标志，定义了，说明相同内容在上一行，替换并打印，如果没有定义，则输出当前行内容
+//  echo, %%b 输出当前行内容，或者写作 echo "%%b", 但不能是 echo %%b, 这会直接输出字符串%%b，而不是变量内容
+//  move tmp "%%a" 替换文件，临时文件替换源文件达到修改的目的
